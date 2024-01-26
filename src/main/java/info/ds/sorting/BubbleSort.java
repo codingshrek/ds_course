4cfc6ec64adb6bb3479703501031fc53139a1e56
Akash Yadav <yadav.akash47@outlook.com>
2024-01-26 14:29:34 +0530

    Basic Sorting

--- src/main/java/info/ds/sorting/BubbleSort.java
+++ src/main/java/info/ds/sorting/BubbleSort.java
@@ -1,2 +1,22 @@
-package info.ds.sorting;public class BubbleSort {
+package info.ds.sorting;
+
+/**
+ * Bubble the largest element in the last.
+ */
+public class BubbleSort {
+    public static void bubbleSort(int[] arr, int n) {
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[i]) swap(arr, i, j);
+            }
+        }
+
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
--- src/main/java/info/ds/sorting/InsertionSort.java
+++ src/main/java/info/ds/sorting/InsertionSort.java
@@ -1,4 +1,25 @@
 package info.ds.sorting;
 
+/**
+ * Select an element(outer loop) , swap it with elements left to until a small no. is encountered.
+ * Idea : array left to current elements is sorted and right is unsorted. Place the current element in the right pos. in sorted segment and shift the remaining to the right.
+ */
 public class InsertionSort {
+    public static void insertionSort(int[] arr, int n) {
+        for (int i = 0; i <= n - 1; i++) {
+            int cur = i;
+            while (cur > 0 && arr[cur] < arr[cur - 1]) {
+                swap(arr, cur, cur - 1);
+                cur--;
+            }
+        }
+
+
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
